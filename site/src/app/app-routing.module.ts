567933ac2b53b38cdf52c27a7f2684f2c2d55be6
Kumodatsu <dragonkumo@gmail.com>
2022-05-28 22:51:30 +0200

    Add games page to navigation

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }             from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
+import { GamesListComponent }   from "./games-list/games-list.component"
 import { MainComponent }        from "./main/main.component"
 
 const routes: Routes = [
   { path: "", redirectTo: "/main", pathMatch: "full" },
 
-  { path: "main", component: MainComponent },
+  { path: "main",  component: MainComponent },
+  { path: "games", component: GamesListComponent },
 ]
 
 @NgModule({
