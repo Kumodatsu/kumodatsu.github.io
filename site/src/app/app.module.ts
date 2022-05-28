92f8d4f8faec3855865456f2d2bd8533bf7dbaea
Kumodatsu <dragonkumo@gmail.com>
2022-05-29 00:10:59 +0200

    Add link button component

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { GamesListComponent } from './games-list/games-list.component';
+import { LinkButtonComponent } from './link-button/link-button.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { GamesListComponent } from './games-list/games-list.component';
     MainComponent,
     NavigationComponent,
     GamesListComponent,
+    LinkButtonComponent
   ],
   imports: [
     BrowserModule,
